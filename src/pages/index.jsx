8214f4245c453ea80f0f4d434ce65d87a093f8da
ibqn <evgen.ibqn@gmail.com>
2022-09-26 00:02:19 +0200

    add viewport tag

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import Features from 'components/features'
 import FeaturesSecond from 'components/features-second'
 import Footer from 'components/footer'
@@ -10,16 +12,23 @@ import Testimonial from 'components/testimonial'
 
 export default function Home() {
   return (
-    <div className="w-full max-w-[1440px] mx-auto bg-white">
-      <Header />
-      <Hero />
-      <Features />
-      <NewInStore />
-      <FeaturesSecond />
-      <Products />
-      <Testimonial />
-      <Newsletter />
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>FurniShop</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="w-full max-w-[1440px] mx-auto bg-white">
+        <Header />
+        <Hero />
+        <Features />
+        <NewInStore />
+        <FeaturesSecond />
+        <Products />
+        <Testimonial />
+        <Newsletter />
+        <Footer />
+      </div>{' '}
+    </>
   )
 }
